085f926137b5544206ecab0b8b2ab7abdad277ad
JITHINK007 <93573046+JITHINK007@users.noreply.github.com>
2023-02-23 21:26:53 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -7,21 +7,25 @@ form.onsubmit = (e)=>{
 }
 
 continueBtn.onclick = ()=>{
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/signup.php", true);
-    xhr.onload = ()=>{
-      if(xhr.readyState === XMLHttpRequest.DONE){
-          if(xhr.status === 200){
-              let data = xhr.response;
-              if(data === "success"){
-                location.href="users.php";
-              }else{
-                // errorText.style.display = "block";
-                // errorText.textContent = data;
-              }
-          }
-      }
-    }
-    let formData = new FormData(form);
-    xhr.send(formData);
+    location.href="chat.html";
 }
+
+// continueBtn.onclick = ()=>{
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST", "php/signup.php", true);
+//     xhr.onload = ()=>{
+//       if(xhr.readyState === XMLHttpRequest.DONE){
+//           if(xhr.status === 200){
+//               let data = xhr.response;
+//               if(data === "success"){
+//                 location.href="users.php";
+//               }else{
+//                 // errorText.style.display = "block";
+//                 // errorText.textContent = data;
+//               }
+//           }
+//       }
+//     }
+//     let formData = new FormData(form);
+//     xhr.send(formData);
+// }
